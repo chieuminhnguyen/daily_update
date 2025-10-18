6dd54c41e304739bab5a91249d869a45a971e463
= <=>
2025-10-18 15:53:38 +0700

    Sua bai tap 17/10/2025 #1

---
+++ reverseString.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int n = 0;
+int arr[100];
+
+void reverseString2(char *s)
+{
+    if(*s != '\0')
+    {
+        reverseString2(s + 1);
+        arr[n] = *s;
+        n += 1;
+    }
+    if(n == strlen(s))
+    {
+        s[strlen(s)] = '\0';
+    }
+}
+
+
+int main()
+{
+    char s[100];
+    scanf("%s", s);
+    reverseString2(s);
+
+    for(int i = 0; *(arr + i) != '\0'; i++)
+    {
+        printf("%c",*(arr + i));
+    }
+    return 0;
+}
